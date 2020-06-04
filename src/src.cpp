1324b1add442d8c716e847afa4fa42b721e0ea6f
takashi imagire <imagire@gmail.com>
2020-06-04 17:56:29 +0900

    add files

---
+++ src/src.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <thread>
+
+int main()
+{
+repeat:
+    std::cout << "credit\n";
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+
+    std::cout << "title\n";
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+
+    std::cout << "opening\n";
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+
+    std::cout << "demo\n";
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+    
+    goto repeat;
+}
+
