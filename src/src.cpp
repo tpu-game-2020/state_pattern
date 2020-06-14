2cf55d23b19de0a52db5659d185c9192b35baf87
nuclearyui <65208421+nuclearyui@users.noreply.github.com>
2020-06-14 23:43:00 +0900

    Update src.cpp

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,61 @@
-﻿#include <iostream>
+#include <iostream>
 #include <thread>
+#define CREDIT 0
+#define TITLE 1
+#define OPENING 2
+#define DEMO 3
 
+class startscreen
+{
+public:
+    virtual void 0()
+    virtual void 1()
+    virtual void 2()
+    virtual void 3()
+};
 int main()
 {
-repeat:
-    std::cout << "credit\n";
+    	int count = 0;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+	while (true)
+	{	
+		switch (count % 4)
+		{
+		case CREDIT:
+			Credit();
+			break;
+		case TIME_UTC:
+			Title();
+			break;
+		case OPENING:
+			Opening();
+			break;
+		case DEMO:
+			Demo();
+			break;
+		default:
+			break;
+		}
 
-    std::cout << "title\n";
+		count++;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
-    std::cout << "opening\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-
-    std::cout << "demo\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-    
-    goto repeat;
+		std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+	}
 }
 
+void Credit() 
+{
+	std::cout << "credit\n";
+}
+void Title() 
+{
+	std::cout << "title\n";
+}
+void Opening() 
+{
+	std::cout << "opening\n";
+}
+void Demo() 
+{
+	std::cout << "demo\n";
+}
