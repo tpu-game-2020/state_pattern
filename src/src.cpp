c9155c02cfd423b93e5d28732b1ffe4bbee98f04
SimoLieF <55975272+SimoLieF@users.noreply.github.com>
2020-06-15 02:49:54 +0900

    Update src.cpp

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,27 @@
 ﻿#include <iostream>
 #include <thread>
 
+enum State{
+    CREDIT,TITLE,OPENING,DEMO
+};
+        
 int main()
 {
-repeat:
-    std::cout << "credit\n";
+    while(true){
+        std::cout << "credit\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+        std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
 
-    std::cout << "title\n";
+        std::cout << "title\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+        std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
 
-    std::cout << "opening\n";
+        std::cout << "opening\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+        std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
 
-    std::cout << "demo\n";
+        std::cout << "demo\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-    
-    goto repeat;
-}
+        std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+    }
 
