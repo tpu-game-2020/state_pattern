b61c09b41e81d8e5d2fd73b99c415e61da1630de
1827010 <43464024+1827010@users.noreply.github.com>
2020-06-14 19:38:36 +0900

    課題

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,50 @@
 ﻿#include <iostream>
 #include <thread>
 
+enum Mode 
+{
+	credit,
+	title,
+	opening,
+	demo,
+	null
+};
+
+void State(int mode)
+{
+	switch (mode)
+	{
+	case Mode::credit:
+		std::cout << "credit\n";
+		std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+		break;
+	case Mode::title:
+		std::cout << "title\n";
+		std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+		break;
+	case Mode::opening:
+		std::cout << "opening\n";
+		std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+		break;
+	case  Mode::demo:
+		std::cout << "demo\n";
+		std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+		break;
+	default:
+		break;
+	}
+}
+
+
 int main()
 {
-repeat:
-    std::cout << "credit\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
-    std::cout << "title\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
-    std::cout << "opening\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-
-    std::cout << "demo\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-    
-    goto repeat;
+	while (true)
+	{
+		for (int i = 0; i < (int)Mode::null; i++) 
+		{
+			State(i);
+		}
+	}
 }
+
 
