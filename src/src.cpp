3c269b5c84c5f2fc2b61f75720e00f59a5e83554
Fanxy2 <65218538+Fanxy2@users.noreply.github.com>
2020-06-15 01:00:51 +0900

    Update src.cpp

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,74 @@
 ﻿#include <iostream>
 #include <thread>
 
-int main()
+class State
 {
-repeat:
+public:
+	virtual void Scene() {}
+};
+
+class CreditState :public State
+{
+public:
+	virtual void Scene()
+    {
     std::cout << "credit\n";
 
     std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
+    }
+};
+class TitleState :public State
+{
+public:
+	virtual void Scene()
+	{
     std::cout << "title\n";
 
     std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
+    }
+};
+class OpeningState :public State
+{
+public:
+	virtual void Scene()
+	{
     std::cout << "opening\n";
 
     std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-
+    }
+};
+class DemoState :public State
+{
+public:
+	virtual void Scene()
+	{
     std::cout << "demo\n";
 
     std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-    
-    goto repeat;
-}
+    }
+};
 
+int main()
+{
+	State* state_;
+
+	CreditState credit;
+	TitleState title;
+	OpeningState opening;
+	DemoState demo;
+
+	while (true)
+	{		
+		state_ = &credit;
+		state_->Scene();
+
+		state_ = &title;
+		state_->Scene();
+
+		state_ = &opening;
+		state_->Scene();
+
+		state_ = &demo;
+		state_->Scene();
+	}
+}
