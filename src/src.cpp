b8efbdbebb5ead7964e894f017b3131016a96fa3
yama0322 <55902857+yama0322@users.noreply.github.com>
2020-06-15 03:35:12 +0900

    Update src.cpp

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,51 @@
-﻿#include <iostream>
+#include <iostream>
 #include <thread>
+
+class State
+{
+public:
+	void Credit();
+	void Title();
+	void Opening();
+	void Demo();
+};
+
+void State::Credit()
+{
+	std::cout << "credit\n";
+
+	std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+}
+
+void State::Title()
+{
+	std::cout << "title\n";
+
+	std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+}
+
+void State::Opening()
+{
+	std::cout << "opening\n";
+
+	std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+}
+
+void State::Demo()
+{
+	std::cout << "demo\n";
+
+	std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+}
 
 int main()
 {
-repeat:
-    std::cout << "credit\n";
+	State show;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
-    std::cout << "title\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
-    std::cout << "opening\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-
-    std::cout << "demo\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-    
-    goto repeat;
+	while (1) {
+		show.Credit();
+		show.Title();
+		show.Opening();
+		show.Demo();
+	}
 }
-
